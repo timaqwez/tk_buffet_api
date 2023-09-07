c9d3920746fbf0ceb3d0cbbcd7f02c7fbff778c7
timaqwez <kapkanrayder@gmail.com>
2023-09-07 10:06:17 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
-app = FastAPI(debug=True)
-app.mount('/static', StaticFiles(directory='api/static'), name='static')
-
-
+app = FastAPI()
